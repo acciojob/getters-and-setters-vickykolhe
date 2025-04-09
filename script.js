6672358f45560ceadf1da830a270dcf02aa26e8c
Babusha Kolhe <165826953+vickykolhe@users.noreply.github.com>
2025-04-10 03:14:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(myname,myage){
+		this._name=myname;
+		this._age=myage;
+	}
 
-class Student extends Person {}
+	  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  get age() {
+    return this._age;
+  }
+
+  set age(newAge) {
+    this._age = newAge;
+  }
+}
+
+class Student extends Person {
+	constructor(myname,myage){
+		super(myname,myage);
+	}
+
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	constructor(myname,myage){
+		super(myname,myage);
+	}
+
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+	
+}
 
 // Do not change the code below this line
 window.Person = Person;
